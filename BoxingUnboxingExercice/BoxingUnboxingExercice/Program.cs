5c5c4f5228baa61aef67330b6108ac90788e68ed
JuanjoAvila <juanjo.avila.chavero@gmail.com>
2019-02-10 19:46:37 +0100

    Se añade la clase con los cambios hechos del boxing y unboxing.

---
+++ BoxingUnboxingExercice/BoxingUnboxingExercice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxingExercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 5;
+            Object box = Boxing(value);
+
+            int unboxedValue = Unboxing(box);
+            Console.WriteLine($"The value unboxed: {unboxedValue}");
+            Console.Read();
+        }
+        private static Object Boxing(int value)
+        {
+            Object box = value;
+            return box;
+        }
+        private static int Unboxing(Object box) => (int)box;
+    }
+}
